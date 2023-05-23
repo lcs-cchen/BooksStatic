aae7ac20819f1c28c888cb1f0abb28fe2b972168
cyrus chen <pschen@lcs.on.ca>
2023-05-23 11:41:44 -0400

    Initial Commit

---
+++ BooksStatic/BooksStaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksStaticApp.swift
+//  BooksStatic
+//
+//  Created by Cyrus Chen on 23/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct BooksStaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
